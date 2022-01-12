81a2f25d99928c6ccb659fd5cf295f2ded97b402
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-01-13 01:18:12 +0330

    Scrabble Hand js

--- js/index.js
+++ js/index.js
@@ -33,3 +33,17 @@ function signYourName(obj) {
   return obj;
 }
 console.log(signYourName(mehdi));
+
+const ava = [
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 },
+];
+function maximumScore(tileHand) {
+  return tileHand.reduce((p, c) => p + c.score, 0);
+}
+console.log(maximumScore(tileHand));

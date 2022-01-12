6aad27c6c7b6e8eb0b813adc47d5d8fa1ad3beb6
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-01-13 02:37:47 +0330

    payane shab aval

--- js/index.js
+++ js/index.js
@@ -60,3 +60,68 @@ function triArea(base, height) {
   return (base * height) / 2;
 }
 console.log(triArea(base, height));
+
+let num = -4;
+function addition(num) {
+  return num + 1;
+}
+console.log(addition(num));
+
+const hours = 4;
+function howManySeconds(hours) {
+  return hours * 3600;
+}
+console.log(howManySeconds(hours));
+
+const x = 1;
+const y = 3;
+function remainder(x, y) {
+  if (x > y) {
+    return y;
+  } else if (x < y) {
+    return x;
+  } else {
+    return 0;
+  }
+}
+console.log(remainder(x, y));
+
+const age = 32;
+function calcAge(age) {
+  return age * 365;
+}
+console.log(calcAge(age));
+
+const minutes = 5;
+function convert(minutes) {
+  return minutes * 60;
+}
+console.log(convert(minutes));
+
+let nums = 12;
+function lessThanOrEqualToZero(nums) {
+  if (nums < 0) {
+    return false;
+  } else if ((nums = 0)) {
+    return true;
+  } else {
+    return true;
+  }
+}
+console.log(lessThanOrEqualToZero(nums));
+
+const ab = 12;
+function cubes(ab) {
+  return Math.pow(ab, 3);
+}
+
+console.log(cubes(ab));
+
+let twoPointers = 8;
+let threePointers = 1;
+function points(twoPointers, threePointers) {
+  let two = twoPointers * 2;
+  let three = threePointers * 3;
+  return two + three;
+}
+console.log(points(twoPointers, threePointers));

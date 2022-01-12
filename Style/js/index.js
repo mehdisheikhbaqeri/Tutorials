d3517d860c52adab4004cab91f6c172ad765c3b0
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-01-13 00:33:17 +0330

    Burglary Series (20): Sign Your Name js

--- Style/js/index.js
+++ Style/js/index.js
@@ -1,13 +1,13 @@
-// const drinks = [
-//   { name: "koka", price: 16 },
-//   { name: "pepsi", price: 15 },
-// ];
+const drinks = [
+  { name: "koka", price: 16 },
+  { name: "pepsi", price: 15 },
+];
 
-// function sortDrinkByPrice(drinks) {
-//   return drinks.sort((m, n) => (m.price > n.price ? 1 : -1));
-// }
+function sortDrinkByPrice(drinks) {
+  return drinks.sort((m, n) => (m.price > n.price ? 1 : -1));
+}
 
-// console.log(sortDrinkByPrice(drinks));
+console.log(sortDrinkByPrice(drinks));
 const arr1 = [1, 2, 4];
 const arr2 = [1, 2, 3];
 function isFirstSuperior(arr1, arr2) {
@@ -20,3 +20,16 @@ function isFirstSuperior(arr1, arr2) {
   return false;
 }
 console.log(isFirstSuperior(arr1, arr2));
+
+const mehdi = { yourSignature: "" };
+function signYourName(obj) {
+  // write your code here
+  // don't use a return statement
+  Object.seal(obj);
+
+  // DON'T CHANGE OR REMOVE THE LINES BELOW
+  obj.yourSignature = "Whatever";
+  obj.spouseSignature = "Nice Try";
+  return obj;
+}
+console.log(signYourName(mehdi));

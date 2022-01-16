53557e124615ce494c4a45af2249505b459d7fc7
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-01-17 03:06:20 +0330

    opreators

---
+++ js/js4/opreators.js
@@ -0,0 +1,71 @@
+// math
+// assigment
+// comparison
+// logical
+
+// math
+// += - + * / % **
+
+// assigment
+// = =+ -= *= /=
+// right to left
+
+// comparison
+// == === != !== < > <= >=
+
+// logical
+// && || !
+
+console.log(100 + 0 / 0);
+
+let score = 10;
+score += 5;
+console.log(score);
+
+// 10 mishe chon asain nakardimesh
+
+let n = 30;
+n++;
+console.log(n);
+
+let x = 100;
+
+console.log(x++);
+
+let y = 200;
+console.log(++y);
+
+// type conversion
+// manual
+
+Number();
+String();
+Boolean();
+
+// let num = prompt("enter number!");
+
+// console.log(Number(num));
+
+console.log(5 + null);
+
+console.log("10" - "3");
+// age menha bashe tabdil mikone be number va amal riazio anjam mide
+
+// ...
+
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(Math.max(...arr));
+
+// console.log(5 + null); //5
+// console.log(Number(null)); //0
+
+// console.log("5" + null); //"5null"
+
+// console.log("5" + 3); //53
+// console.log(3 + "5"); //35
+
+// console.log("5" - "2");
+
+// let number = "1" + 1; //11
+// number -= 1; //10 number = number +1
+// console.log(number);

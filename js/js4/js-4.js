06eacb2db874ae50f8fb41d9727c40da8e27a79d
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-01-17 02:56:14 +0330

    string

---
+++ js/js4/js-4.js
@@ -0,0 +1,13 @@
+// string methods
+
+let str1 = "hello";
+let str2 = "hi guys, iam mehdi";
+
+// methods
+
+console.log(str1.charAt(0));
+console.log(str1.charCodeAt(0));
+// (unit cod show)
+console.log(str2.endsWith("mehdi"));
+console.log(str1.repeat(3));
+console.log(str2.split("+"));

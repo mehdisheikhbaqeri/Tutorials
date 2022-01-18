dabfe06f2c1a120d3bfac75aee11c1714b2ae051
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-01-19 02:00:42 +0330

    ave-num

---
+++ js/js6.js/ave-num.js
@@ -0,0 +1,6 @@
+const userFirstNum = Number(prompt("enter first number:"));
+const userSectNum = Number(prompt("enter 2th number:"));
+const userThirdtNum = Number(prompt("enter 3th number:"));
+
+let average = (userFirstNum + userSectNum + userThirdtNum) / 3;
+alert(average);

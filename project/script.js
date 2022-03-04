a56ea9a100e9f30035aa3fb902c7fffe4f57dac5
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-03-04 22:58:18 +0330

    js Dom selector

---
+++ project/script.js
@@ -0,0 +1,29 @@
+let header = document.getElementById("header");
+
+// console.log(header);
+// header.style.color = "red";
+
+let content = document.getElementsByClassName("content");
+
+// console.log(content[1]);
+// content.style.color = "green";
+let paragraphs = document.getElementsByTagName("p");
+// console.log(paragraphs);
+// paragraphs[2].innerText = "mehdi gol e golabe";
+
+let header2 = document.querySelector("#header");
+// header.style.color = "red";
+// let p = document.querySelector("p");
+// p.style.color = "khaki";
+
+// let pS = document.querySelectorAll("p");
+// pS.forEach((p) => {
+//   p.style.color = "green";
+// });
+
+// let content2 = document.querySelectorAll(".content");
+// content2.forEach((content) => {
+//   content.style.color = "blue";
+// });
+
+// Array.from

e2d57ba5fb0e966671bb06449d5a0ffd2fc2552f
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-03-07 00:44:25 +0330

    export and import

---
+++ npm/utils.js
@@ -0,0 +1,19 @@
+let username = "mehdi";
+
+let age = 32;
+
+function double(num) {
+  return num * 2;
+}
+
+// export { username, age };
+export { double };
+
+export let lastName = "baqeri";
+
+// export default(behtarin halat)
+
+let number = 33;
+export let username2 = "mehdi";
+
+export default number;

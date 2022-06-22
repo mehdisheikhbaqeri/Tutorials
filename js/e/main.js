172bab7211d015e3ac62be267cf2c1aa4cf96f04
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-06-22 21:18:25 +0430

    base js

---
+++ js/e/main.js
@@ -0,0 +1,5 @@
+let h1Elm = document.querySelector("h1");
+
+function clickFun(randBg) {
+  h1Elm.style.color = "#5c7aa2";
+}
---
+++ js/is-has/is-has.js
@@ -0,0 +1,7 @@
+let hasProfile = false;
+
+setTimeout(function () {
+  if (!hasProfile) {
+    alert("Upload Your Profile Picture");
+  }
+}, 5000);

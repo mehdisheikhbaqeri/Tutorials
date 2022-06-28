14204efa40a30ecb252c39e8f95111b1ce6d3856
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-06-28 19:45:31 +0430

    login end

---
+++ js/MP/mp2/main.js
@@ -0,0 +1,20 @@
+let userNameInput = document.querySelector(".username");
+let passwordInput = document.querySelector(".password");
+let modal = document.querySelector(".modal");
+function dataValidation() {
+  let usernameValue = userNameInput.value;
+  let passwordValue = passwordInput.value;
+
+  if (usernameValue.length < 12 || passwordValue < 8) {
+    // alert("error");
+    modal.style.display = "inline";
+  } else {
+    modal.style.background = "green";
+    modal.textContent = "Sing in is OK!!!";
+    modal.style.display = "inline";
+    modal.style.right = "40vw";
+  }
+  setTimeout(function () {
+    modal.style.display = "none";
+  }, 3000);
+}

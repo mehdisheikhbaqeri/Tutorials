fa0bb8b48fa746ff641b4a94961411221995d6d2
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-07-18 21:01:16 +0430

    fixed bug and end

--- js/MP/Temp Conversion/main.js
+++ js/MP/Temp Conversion/main.js
@@ -1,30 +1,45 @@
 const CantDeg = document.querySelector(".C");
 const FarenDeg = document.querySelector(".F");
-const converter = document.querySelector(".converte");
+const converter = document.querySelector("#converter");
 const result = document.querySelector(".result");
 const convertButton = document.querySelector(".convertButton");
 const resetButton = document.querySelector(".resetButton");
 const changeButton = document.querySelector(".changeButton");
 
-function conver() {}
-function reset() {
-  result.innerHTML = "";
-  converter.value = ""; ///Bug
-}
-function change() {
-  if (CantDeg.innerHTML === "°C") {
-    CantDeg.innerHTML = "°F";
-    FarenDeg.innerHTML = "°C";
-    // converter.value = "°C";
-    document.title = "°F to °C";
+function convert() {
+  if (converter.value === "") {
+    result.textContent = "Insert correct value";
+    result.style.color = "#992233";
   } else {
-    CantDeg.innerHTML = "°C";
-    FarenDeg.innerHTML = "°F";
-    // converter.setAttribute("placeholder", "°C");
-    document.title = "°C to °F";
+    if (CantDeg.textContent === "°C") {
+      let finalValue = converter.value * 1.8 + 32;
+      result.textContent =
+        converter.value + "°C  ->  " + finalValue.toFixed(2) + "°F";
+    } else {
+      let finalValueplus = (converter.value - 32) / 1.8;
+      result.textContent =
+        converter.value + "°F  ->  " + finalValueplus.toFixed(2) + "°C";
+    }
   }
 }
-
-convertButton.addEventListener("click", converter);
+function reset() {
+  result.textContent = "";
+  converter.setAttribute("value", "");
+}
+function change() {
+  if (CantDeg.textContent === "°C") {
+    CantDeg.textContent = "°F";
+    FarenDeg.textContent = "°C";
+    converter.setAttribute("placeholder", "°F");
+    document.title = "°F to °C";
+  } else {
+    CantDeg.textContent = "°C";
+    FarenDeg.textContent = "°F";
+    document.title = "°C to °F";
+    converter.setAttribute("placeholder", "°C");
+  }
+}
+console.log(converter);
+convertButton.addEventListener("click", convert);
 resetButton.addEventListener("click", reset);
 changeButton.addEventListener("click", change);

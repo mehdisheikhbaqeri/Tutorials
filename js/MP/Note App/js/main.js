192cc07340a4ba851c1d2355d2865731bf300be7
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-07-26 16:52:48 +0430

    start note app

---
+++ js/MP/Note App/js/main.js
@@ -0,0 +1,5 @@
+const input = document.querySelector("#input-field");
+const btnSave = document.querySelector("#btn-save");
+const btnDel = document.querySelector("#btn-delete");
+const colors = document.querySelectorAll(".color-box");
+const note = document.querySelector("#listed");

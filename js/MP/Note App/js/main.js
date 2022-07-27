1a96054dbc909aed338149ea27cc946dcd8b07db
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-07-27 16:25:56 +0430

    color box and input

--- js/MP/Note App/js/main.js
+++ js/MP/Note App/js/main.js
@@ -3,3 +3,19 @@ const btnSave = document.querySelector("#btn-save");
 const btnDel = document.querySelector("#btn-delete");
 const colors = document.querySelectorAll(".color-box");
 const note = document.querySelector("#listed");
+
+colors.forEach(function (colorBox) {
+  //   console.log(colorBox);
+
+  colorBox.addEventListener("click", function (e) {
+    console.log(e.target);
+
+    let maincolor = e.target.style.backgroundColor;
+    input.style.backgroundColor = maincolor;
+  });
+});
+
+btnDel.addEventListener("click", function () {
+  input.value = "";
+  input.style.backgroundColor = "#FFF";
+});

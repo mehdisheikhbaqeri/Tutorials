f69b3aaac763cc38602fe036c62e0eb9467b6aff
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-07-27 18:12:42 +0430

    note app end

--- js/MP/Note App/js/main.js
+++ js/MP/Note App/js/main.js
@@ -8,14 +8,43 @@ colors.forEach(function (colorBox) {
   //   console.log(colorBox);
 
   colorBox.addEventListener("click", function (e) {
-    console.log(e.target);
+    // console.log(e.target);
 
     let maincolor = e.target.style.backgroundColor;
     input.style.backgroundColor = maincolor;
   });
 });
 
+function newNote() {
+  let newNoteDev = document.createElement("div");
+  newNoteDev.className = "card shadow-sm rounded";
+
+  let inputColore = input.style.backgroundColor;
+  newNoteDev.style.backgroundColor = inputColore;
+
+  let newNoteP = document.querySelector("p");
+  newNoteP.className = "card-text p-3";
+  newNoteP.textContent = input.value;
+
+  newNoteDev.append(newNoteP);
+  note.append(newNoteDev);
+
+  input.value = "";
+  console.log(newNoteP);
+}
+
 btnDel.addEventListener("click", function () {
   input.value = "";
   input.style.backgroundColor = "#FFF";
 });
+
+input.addEventListener(
+  "keydown",
+  function (e) {
+    console.log(e.keyCode);
+    if (e.keyCode == 13) {
+      newNote();
+    }
+  },
+  false
+);

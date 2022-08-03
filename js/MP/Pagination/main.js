0b49c64699925f4a63750f2424c8326a8c5679b0
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-08-03 22:28:35 +0430

    rowsCount & currentPage added

---
+++ js/MP/Pagination/main.js
@@ -0,0 +1,56 @@
+const listItems = [
+  { id: 1, name: "Mehdi", family: "Baqeri" },
+  { id: 2, name: "Anahid", family: "Hemmat" },
+  { id: 3, name: "Hasan", family: "Saber" },
+  { id: 4, name: "Ashkan", family: "Delha" },
+  { id: 5, name: "Hasan", family: "Ghahreman Zadeh" },
+  { id: 6, name: "Kiana", family: "Behrouz Far" },
+  { id: 7, name: "Kimmia", family: "Behrouz Far" },
+  { id: 8, name: "Alireza", family: "Dalvand" },
+  { id: 9, name: "Pourya", family: "Gazizadeh" },
+  { id: 10, name: "Arad", family: "Hasanpur" },
+  { id: 11, name: "Benyamin", family: "naydi" },
+  { id: 12, name: "David", family: "Beckham" },
+  { id: 13, name: "Manchester", family: "United" },
+  { id: 14, name: "Perspolis", family: "Iran" },
+  { id: 15, name: "Coffee", family: "Espersso" },
+  { id: 16, name: "Caramel", family: "Makiato" },
+  { id: 17, name: "B", family: "Plus" },
+  { id: 18, name: "Podcast", family: "Ravcast" },
+  { id: 19, name: "Podcast", family: "Majon" },
+  { id: 20, name: "Podcast", family: "Channel B" },
+  { id: 21, name: "Podcast", family: "Rukh" },
+  { id: 22, name: "Podcast", family: "Dialogcast" },
+];
+
+let userListContainer = document.querySelector("#list");
+let paginationContainer = document.querySelector("#pagination");
+
+let currentPage = 1;
+let rowsCount = 5;
+
+function displayUesrsList(
+  allUesrsArray,
+  usersContainer,
+  rowsCount,
+  currentPage
+) {
+  usersContainer.innerHTML = "";
+
+  let endIndex = rowsCount * currentPage;
+  let startIndex = endIndex - rowsCount;
+
+  let paginatedUsers = allUesrsArray.slice(startIndex, endIndex);
+
+  paginatedUsers.forEach(function (user) {
+    let userElement = document.createElement("div");
+    userElement.classList.add("item");
+    userElement.innerHTML = user.name + " " + user.family;
+
+    usersContainer.appendChild(userElement);
+  });
+
+  console.log(paginatedUsers);
+}
+
+displayUesrsList(listItems, userListContainer, rowsCount, currentPage);

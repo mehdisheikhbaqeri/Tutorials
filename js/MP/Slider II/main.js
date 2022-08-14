907a3d6a36fe83583d16fdd0455116576e97a125
Mehdi <mehdi7baqeri@gmail.com>
2022-08-14 21:02:28 +0430

    add point event listener in js

--- js/MP/Slider II/main.js
+++ js/MP/Slider II/main.js
@@ -4,21 +4,30 @@ let points = document.querySelectorAll(".points>span");
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
 let active = 0;
-let timer = 2500;
+let timer = 5000;
 
 // function
 
-function classSwitcher(){
-    slides.forEach(slides=>slides.classList.remove('active'));
-    points.forEach(point=>point.classList.remove('active'));
-    slides[active].classList.add("active");
-    points[active].classList.add("active");
+function classSwitcher() {
+  slides.forEach((slides) => slides.classList.remove("active"));
+  points.forEach((point) => point.classList.remove("active"));
+  slides[active].classList.add("active");
+  points[active].classList.add("active");
 }
 
 let goNext = () => {
   active = active == slides.length - 1 ? 0 : active + 1;
-classSwitcher()
+  classSwitcher();
 };
 
 // setInterval
 setInterval(goNext, timer);
+
+// point event
+
+points.forEach((point, index) => {
+  point.addEventListener("click", (e) => {
+    active = index;
+    classSwitcher()
+  });
+});
